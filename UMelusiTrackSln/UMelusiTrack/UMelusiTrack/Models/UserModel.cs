99f65ed1f30c1bfcce31e36d32dbedfcbe35b373
Ishmael Sikhikhi <101715069+WebDeveloperDesigner2022@users.noreply.github.com>
2022-06-21 19:37:20 +0200

    Login and sign up database

---
+++ UMelusiTrackSln/UMelusiTrack/UMelusiTrack/Models/UserModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using UMelusiTrack.Services;
+using Xamarin.Forms;
+
+namespace UMelusiTrack.Models
+{
+    public class UserModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string username;
+        UmelusiDB umelusiDB;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Username"));
+            }
+        }
+
+        public string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+
+        public ICommand SubmitCommand { get; set; }
+
+        public UserModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+        }
+
+        public void OnSubmit()
+        {
+            if (string.IsNullOrEmpty(Username) && string.IsNullOrEmpty(Password))
+            {
+                MessagingCenter.Send(this, "Login Alert", "Please fill-up the form");
+            }
+            else if (string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+            {
+                MessagingCenter.Send(this, "Login Alert", "Please enter username the form");
+            }
+            else if (!string.IsNullOrEmpty(Username) && string.IsNullOrEmpty(Password))
+            {
+                MessagingCenter.Send(this, "Login Alert", "Please enter password the form");
+            }
+            else
+            {
+                MessagingCenter.Send(this, "Login Alert", Username + " & " + Password);
+                umelusiDB = new UmelusiDB();
+
+
+            }
+
+        }
+    }
+}
--- UMelusiTrackSln/UMelusiTrack/UMelusiTrack/Views/SignPage.xaml.cs
+++ UMelusiTrackSln/UMelusiTrack/UMelusiTrack/Views/SignPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using UMelusiTrack.Models;
+using UMelusiTrack.Services;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,14 +13,28 @@ namespace UMelusiTrack
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SignPage : ContentPage
     {
+        public UserModel userModel;
         public SignPage()
         {
             InitializeComponent();
-        }
+            userModel = new UserModel();
+            
+            MessagingCenter.Subscribe<UserModel, string>(this, "Login Alert", (sender, username) =>
+            {
+                DisplayAlert("", username, "Ok");
+            });
+            this.BindingContext = userModel;
 
-        private void Login_Clicked(object sender, EventArgs e)
-        {
+            entUsername.Completed += (object sender, EventArgs e) =>
+            {
+                entPassword.Focus();
+            };
 
+            entPassword.Completed += (object sender, EventArgs e) =>
+            {
+                userModel.SubmitCommand.Execute(null);
+
+            };
         }
 
         private async void Signup_Clicked(object sender, EventArgs e)
