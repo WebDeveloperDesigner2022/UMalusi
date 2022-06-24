0c71ce5a1e968bddddd6905023e16c4212fa1330
ASIPHE NCOMBO <4270171@myuwc.ac.za>
2022-06-24 12:00:18 +0200

    CAREATED A SHOPPING PAGE

---
+++ UMelusiTrackSln/UMelusiTrack/UMelusiTrack/Views/ShopPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UMelusiTrack
+{
+    public partial class ShopPage : ContentPage
+    {
+        public ShopPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void cancelBtn(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private void saveBtn(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
