30331f34f16b1d1290a97a20e152699f606baf1a
WebDeveloperDesigner2022 <4275876@myuwc.ac.za>
2022-07-01 15:40:57 +0200

    Added shopping database

---
+++ UMelusiTrackSln/UMelusiTrack/UMelusiTrack/Models/OrderingModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace UMelusiTrack.Models
+{
+    public class OrderingModels
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Quantity { get; set; }
+        public string Refence_number { get; set; }
+        public string Delivery_options { get; set; }
+        public string Delivery_address { get; set; }
+        public string Contact_number { get; set; }
+        public string Email_address { get; set; }
+    }
+}
---
+++ UMelusiTrackSln/UMelusiTrack/UMelusiTrack/Services/OrderingDb.cs
@@ -0,0 +1,58 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UMelusiTrack.Models;
+
+namespace UMelusiTrack.Services
+{
+    public class OrderingDb
+    {
+        static SQLiteAsyncConnection Database;
+
+        public static readonly AsyncLazy<OrderingDb> Instance = new AsyncLazy<OrderingDb>(async () =>
+        {
+            var instance = new OrderingDb();
+            CreateTableResult result = await Database.CreateTableAsync<OrderingModels>();
+            return instance;
+        });
+
+        public OrderingDb()
+        {
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        }
+
+        public Task<List<OrderingModels>> GetItemsAsync()
+        {
+            return Database.Table<OrderingModels>().ToListAsync();
+        }
+
+        public Task<List<OrderingModels>> GetItemsNotDoneAsync()
+        {
+            return Database.QueryAsync<OrderingModels>("SELECT * FROM [livestock]");
+        }
+
+        public Task<OrderingModels> GetItemAsync(int id)
+        {
+            return Database.Table<OrderingModels>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(OrderingModels item)
+        {
+            if (item.Id != 0)
+            {
+                return Database.UpdateAsync(item);
+            }
+            else
+            {
+                return Database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(OrderingModels item)
+        {
+            return Database.DeleteAsync(item);
+        }
+    }
+}
