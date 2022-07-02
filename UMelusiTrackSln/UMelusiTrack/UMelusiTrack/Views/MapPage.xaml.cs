270bc50dc7e31af21a4dcaf9de20eba8c51618f2
Sinobom-Plaatjie <4270448@myuwc.ac.za>
2022-07-02 11:20:26 +0200

    Added Locate Page

--- UMelusiTrackSln/UMelusiTrack/UMelusiTrack/Views/MapPage.xaml.cs
+++ UMelusiTrackSln/UMelusiTrack/UMelusiTrack/Views/MapPage.xaml.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace UMelusiTrack
@@ -15,6 +16,40 @@ namespace UMelusiTrack
         public MapPage()
         {
             InitializeComponent();
+            DisplayCurrentLocation();
+        }
+        public async void DisplayCurrentLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Position p = new Position(location.Latitude, location.Longitude);
+                    MapSpan mapSpan = MapSpan.FromCenterAndRadius(p, Distance.FromKilometers(.444));
+                    map.MoveToRegion(mapSpan);
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
         }
     }
 }
+    
