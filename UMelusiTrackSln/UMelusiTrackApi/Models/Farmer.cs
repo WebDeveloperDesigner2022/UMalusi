948f450069148655c3d7a023e83709805dd6cf3f
Sinobom-Plaatjie <4270448@myuwc.ac.za>
2022-07-05 14:22:09 +0200

    Added All models for database

---
+++ UMelusiTrackSln/UMelusiTrackApi/Models/Farmer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UMelusiTrackApi.Models
+{
+    [Table("Farmer")]
+    public class Farmer
+    {
+        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int FarmerId { get; set; }
+        public string Names { get; set; }
+        public string Surname { get; set; }
+        public string Useraname { get; set; }
+        public string Password { get; set; }
+        
+        [ForeignKey("Authentication")]
+        public int AuthenticationId { get; set; }
+
+        public Authentication? Authentication { get; set; }
+
+        public ICollection<Livestock>? Livestocks { get; set; }
+
+
+
+    }
+}
