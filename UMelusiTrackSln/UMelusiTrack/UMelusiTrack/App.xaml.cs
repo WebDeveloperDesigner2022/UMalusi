13c2667c31526b88ac07ecccf556c5a4f2ade84b
ASIPHE NCOMBO <4270171@myuwc.ac.za>
2022-07-29 14:43:42 +0200

    Merging

--- UMelusiTrackSln/UMelusiTrack/UMelusiTrack/App.xaml.cs
+++ UMelusiTrackSln/UMelusiTrack/UMelusiTrack/App.xaml.cs
@@ -10,7 +10,7 @@ namespace UMelusiTrack
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new SignPage())
+            MainPage = new NavigationPage(new MainPage2())
             {//6FBF7B
                 //Color mintGreen =  Color.FromHex("");
                 BackgroundColor = Color.FromHex("#6FBF7B"), BarBackgroundColor = Color.FromHex("#6FBF7B"),
