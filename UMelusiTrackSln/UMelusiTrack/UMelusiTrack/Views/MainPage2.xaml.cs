83a8e62390dcc5d170b6d19b20a6612fb1496955
Sinobom-Plaatjie <4270448@myuwc.ac.za>
2022-08-08 10:53:41 +0200

    .

--- UMelusiTrackSln/UMelusiTrack/UMelusiTrack/Views/MainPage2.xaml.cs
+++ UMelusiTrackSln/UMelusiTrack/UMelusiTrack/Views/MainPage2.xaml.cs
@@ -14,11 +14,13 @@ namespace UMelusiTrack
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage2 : FlyoutPage
     {
+        //Map map = new Map();
         public MainPage2()
         {
             InitializeComponent();
             flyout.listView.ItemSelected += OnSelectedItem;
             DisplayCurrentLocation();
+            
         }
         void OnSelectedItem(object sender, SelectedItemChangedEventArgs e)
         {
@@ -43,8 +45,11 @@ namespace UMelusiTrack
                     MapSpan mapSpan = MapSpan.FromCenterAndRadius(p, Distance.FromKilometers(.444));
                     map.MoveToRegion(mapSpan);
                     Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    map.Pins.Add(pin);
+
                 }
             }
+           
             catch (FeatureNotSupportedException fnsEx)
             {
                 // Handle not supported on device exception
@@ -62,7 +67,21 @@ namespace UMelusiTrack
                 // Unable to get location
             }
         }
+        /* public async void DisplayLivestockPosition()
+         {
 
+         }
+        */
+        Pin pin = new Pin
+        {
+            Label = "Cow11",
+            Address = "The city with a boardwalk",
+            Type = PinType.Place,
+            Position = new Position(36.9628066, -122.0194722)
+        };
+        
+        
+        
         private void ManageButton(object sender, EventArgs e)
         {
 
